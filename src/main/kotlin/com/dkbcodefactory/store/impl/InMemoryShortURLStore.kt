7762b58526bef2a597612b30c48166b1cf57d0af
Pranav Sukumara Kurup <pranavs.nkm@gmail.com>
2022-09-08 18:42:12 +0200

    Updated exception, Added test cases for InMemoryShortURLStore and added InMemoryShortURLStore

--- src/main/kotlin/com/dkbcodefactory/store/impl/InMemoryShortURLStore.kt
+++ src/main/kotlin/com/dkbcodefactory/store/impl/InMemoryShortURLStore.kt
@@ -1,19 +1,45 @@
 package com.dkbcodefactory.store.impl
 
+import com.dkbcodefactory.exception.KeyNotFoundException
+import com.dkbcodefactory.exception.URLAlreadyExists
+import com.dkbcodefactory.store.KeyStore
 import com.dkbcodefactory.store.ShortURLStore
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.util.concurrent.ConcurrentHashMap
 
-class InMemoryShortURLStore : ShortURLStore {
+class InMemoryShortURLStore(private val keyStore: KeyStore) : ShortURLStore {
+    companion object {
+        private val KEY_URL_STORE = ConcurrentHashMap<String, String>()
+        fun clear() {
+            KEY_URL_STORE.clear()
+        }
+    }
+
     override fun get(key: String): Mono<String> {
-        TODO("Not yet implemented")
+        if (!KEY_URL_STORE.containsKey(key)) {
+            throw KeyNotFoundException(key)
+        }
+        return Mono.just(KEY_URL_STORE[key]!!)
     }
 
     override fun add(url: String): Mono<String> {
-        TODO("Not yet implemented")
+        if (KEY_URL_STORE.containsValue(url)) {
+            val first = KEY_URL_STORE.filter { entry -> entry.value == url }.map { entry -> entry.key }.first()
+            return Mono.error(URLAlreadyExists(first, url))
+        }
+        var key: String?
+        while (true) {
+            key = keyStore.getKey()
+            if (KEY_URL_STORE.containsKey(key))
+                continue
+            KEY_URL_STORE[key] = url
+            break
+        }
+        return Mono.just(key!!)
     }
 
     override fun getStoreValues(): Flux<Pair<String, String>> {
-        TODO("Not yet implemented")
+        return Flux.fromStream(KEY_URL_STORE.entries.stream().map { entry -> Pair(entry.key, entry.value) })
     }
 }
--- src/test/kotlin/com/dkbcodefactory/store/impl/InMemoryShortURLStoreTest.kt
+++ src/test/kotlin/com/dkbcodefactory/store/impl/InMemoryShortURLStoreTest.kt
@@ -2,16 +2,23 @@ package com.dkbcodefactory.store.impl
 
 import com.dkbcodefactory.exception.KeyNotFoundException
 import com.dkbcodefactory.exception.URLAlreadyExists
+import com.dkbcodefactory.store.KeyStore
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import org.springframework.util.Assert
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.stream.Collectors
 
 internal class InMemoryShortURLStoreTest {
 
     @Test
     fun getWithMissingKey() {
+        InMemoryShortURLStore.clear()
+        val keyStore = Mockito.mock(KeyStore::class.java)
         var error = Assertions.assertThrows(KeyNotFoundException::class.java) {
-            var shortURLStore = InMemoryShortURLStore()
+            var shortURLStore = InMemoryShortURLStore(keyStore)
             shortURLStore.get("test")
         }
         Assert.isTrue(
@@ -22,7 +29,11 @@ internal class InMemoryShortURLStoreTest {
 
     @Test
     fun getWithExistingKey() {
-        var shortURLStore = InMemoryShortURLStore()
+        InMemoryShortURLStore.clear()
+        val keyStore = Mockito.mock(KeyStore::class.java)
+        var shortURLStore = InMemoryShortURLStore(keyStore)
+        `when`(keyStore.getKey()).thenReturn("test")
+        shortURLStore.add("test").block()
         val result = shortURLStore.get("test")
         Assert.notNull(result, "Expected result is not null")
         Assert.notNull(result.block(), "Expected result is not null")
@@ -30,10 +41,31 @@ internal class InMemoryShortURLStoreTest {
     }
 
     @Test
+    fun addWithExistingKey() {
+        InMemoryShortURLStore.clear()
+        val keyStore = Mockito.mock(KeyStore::class.java)
+        var shortURLStore = InMemoryShortURLStore(keyStore)
+        val i = AtomicInteger(0)
+        `when`(keyStore.getKey()).thenReturn("test-${i.get()}")
+        shortURLStore.add("test").block()
+        `when`(keyStore.getKey()).then { "test-${i.getAndIncrement()}" }
+        shortURLStore.add("test2").block()
+        val result = shortURLStore.get("test-0")
+        Assert.notNull(result, "Expected result is not null")
+        Assert.notNull(result.block(), "Expected result is not null")
+        Assert.isTrue(result.block() == "test", "Invalid result on passed key")
+    }
+
+    @Test
     fun addExistingValue() {
+        InMemoryShortURLStore.clear()
+        val keyStore = Mockito.mock(KeyStore::class.java)
+
         var error = Assertions.assertThrows(URLAlreadyExists::class.java) {
-            var shortURLStore = InMemoryShortURLStore()
-            shortURLStore.add("test")
+            var shortURLStore = InMemoryShortURLStore(keyStore)
+            `when`(keyStore.getKey()).thenReturn("test")
+            shortURLStore.add("test").block()
+            shortURLStore.add("test").block()
         }
         Assert.isTrue(
             error.message == "Already url: 'test' with key: 'test' exists.",
@@ -43,11 +75,28 @@ internal class InMemoryShortURLStoreTest {
 
     @Test
     fun addNonExistingValue() {
-        var shortURLStore = InMemoryShortURLStore()
+        InMemoryShortURLStore.clear()
+        val keyStore = Mockito.mock(KeyStore::class.java)
+        `when`(keyStore.getKey()).thenReturn("test")
+        var shortURLStore = InMemoryShortURLStore(keyStore)
         val result = shortURLStore.add("test")
         Assert.notNull(result, "Expected result is not null")
         Assert.notNull(result.block(), "Expected result is not null")
         Assert.isTrue(result.block() == "test", "Invalid result on passed key")
 
+    }
+
+    @Test
+    fun getStoreValuesTest() {
+        InMemoryShortURLStore.clear()
+        val keyStore = Mockito.mock(KeyStore::class.java)
+        var shortURLStore = InMemoryShortURLStore(keyStore)
+        val i = AtomicInteger(0)
+        `when`(keyStore.getKey()).then{"test-${i.getAndIncrement()}"}
+        shortURLStore.add("test1").block()
+        shortURLStore.add("test2").block()
+        val result = shortURLStore.getStoreValues().collect(Collectors.toList()).block()
+        Assert.notNull(result, "Expected result is not null")
+        Assert.isTrue(result!!.size == 2, "Expected result is not empty")
     }
 }
