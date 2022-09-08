a1f82ce007459ce432b856fd0065c142001098d7
Pranav Sukumara Kurup <pranavs.nkm@gmail.com>
2022-09-08 10:10:10 +0200

    Added in memory key store and tests updated

--- src/main/kotlin/com/dkbcodefactory/store/impl/InMemoryKeyStore.kt
+++ src/main/kotlin/com/dkbcodefactory/store/impl/InMemoryKeyStore.kt
@@ -1,15 +1,46 @@
 package com.dkbcodefactory.store.impl
 
+import com.dkbcodefactory.exception.KeyGenerationException
 import com.dkbcodefactory.store.KeyStore
+import com.dkbcodefactory.util.KeyGenerator
 import reactor.core.publisher.Flux
+import java.util.concurrent.LinkedBlockingDeque
+import java.util.concurrent.TimeUnit
 
-class InMemoryKeyStore : KeyStore {
+class InMemoryKeyStore(private val keyGenerator: KeyGenerator, private val cache: Int) : KeyStore {
+    companion object {
+        private val IN_MEMORY_STORE = LinkedBlockingDeque<String>()
+
+        fun clear() {
+            IN_MEMORY_STORE.clear()
+        }
+    }
+
+    fun populateKeys() {
+        var count = IN_MEMORY_STORE.size
+        var iterations = 0
+        while (count < cache / 2) {
+            iterations++
+            keyGenerator.generateKeys(cache - count).forEach { key ->
+                if (!IN_MEMORY_STORE.contains(key)) {
+                    IN_MEMORY_STORE.add(key)
+                }
+            }
+            count = IN_MEMORY_STORE.size
+            if (iterations > 10 * cache) {
+                throw KeyGenerationException("Error populating cache")
+            }
+        }
+    }
+
     override fun getKey(): String {
-        TODO("Not yet implemented")
+        populateKeys()
+        return IN_MEMORY_STORE.poll(1, TimeUnit.MINUTES)!!
     }
 
     override fun getStoreValues(): Flux<String> {
-        TODO("Not yet implemented")
+        populateKeys()
+        return Flux.fromStream(IN_MEMORY_STORE.stream())
     }
 
 }
--- src/test/kotlin/com/dkbcodefactory/store/impl/InMemoryKeyStoreTest.kt
+++ src/test/kotlin/com/dkbcodefactory/store/impl/InMemoryKeyStoreTest.kt
@@ -1,25 +1,84 @@
 package com.dkbcodefactory.store.impl
 
+import com.dkbcodefactory.exception.KeyGenerationException
+import com.dkbcodefactory.util.KeyGenerator
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
 import org.springframework.util.Assert
+import java.util.stream.Collectors
 
 internal class InMemoryKeyStoreTest {
 
     @Test
     fun getKeyTest() {
-        val keyStore = InMemoryKeyStore()
+        InMemoryKeyStore.clear()
+        val keyGenerator = Mockito.mock(KeyGenerator::class.java)
+        var inputData =
+            setOf("test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test9", "test10")
+        Mockito.`when`(keyGenerator.generateKeys(10)).thenReturn(inputData)
+        val keyStore = InMemoryKeyStore(keyGenerator, 10)
         val key = keyStore.getKey()
         Assert.notNull(key, "Short key string from store is null")
-        Assert.isTrue(key.trim().isNotEmpty(), "Short key string from store is empty")
+        Assert.isTrue(key == inputData.first(), "Short key string from store is empty")
     }
 
 
     @Test
     fun getStoreValuesTest() {
-        val keyStore = InMemoryKeyStore()
+        InMemoryKeyStore.clear()
+        val keyGenerator = Mockito.mock(KeyGenerator::class.java)
+        var inputData =
+            setOf("test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test9", "test10")
+        Mockito.`when`(keyGenerator.generateKeys(10)).thenReturn(inputData)
+        val keyStore = InMemoryKeyStore(keyGenerator, 10)
         val values = keyStore.getStoreValues()
         Assert.notNull(values, "values in store is null")
-        Assert.notNull(values.collectList().block(), "values in store is null")
-        Assert.isTrue(!values.collectList().block()?.isEmpty()!!, "values in store is empty")
+        val list = values.toStream().collect(Collectors.toSet())
+        Assert.notNull(list, "values in store is null")
+        Assert.isTrue(!list?.isEmpty()!!, "values in store is empty")
+        Assert.isTrue(inputData == list, "values in store is empty")
+    }
+
+    @Test
+    fun keyGenerationException() {
+        InMemoryKeyStore.clear()
+        var error = Assertions.assertThrows(KeyGenerationException::class.java) {
+            val keyGenerator = Mockito.mock(KeyGenerator::class.java)
+            var inputData = HashSet<String>()
+            Mockito.`when`(keyGenerator.generateKeys(10)).thenReturn(inputData)
+            val keyStore = InMemoryKeyStore(keyGenerator, 10)
+            keyStore.getKey()
+        }
+        Assert.isTrue(error.message == "Error populating cache", "Error message is incorrect")
+
+    }
+
+    @Test
+    fun clearkeyGeneration() {
+        InMemoryKeyStore.clear()
+        val keyGenerator = Mockito.mock(KeyGenerator::class.java)
+        var inputData = setOf("test1", "test2", "test3", "test4", "test5", "test6", "test7", "test8", "test9", "test10")
+        Mockito.`when`(keyGenerator.generateKeys(10)).thenReturn(inputData)
+        val keyStore = InMemoryKeyStore(keyGenerator, 10)
+        val values = keyStore.getStoreValues()
+        Assert.notNull(values, "values in store is null")
+        val list = values.toStream().collect(Collectors.toSet())
+        Assert.notNull(list, "values in store is null")
+        Assert.isTrue(!list?.isEmpty()!!, "values in store is empty")
+        Assert.isTrue(inputData == list, "values in store is empty")
+
+
+        InMemoryKeyStore.clear()
+        inputData = HashSet<String>()
+        Mockito.`when`(keyGenerator.generateKeys(10)).thenReturn(inputData)
+        var error = Assertions.assertThrows(KeyGenerationException::class.java) {
+            val keyGenerator = Mockito.mock(KeyGenerator::class.java)
+            var inputData = HashSet<String>()
+            Mockito.`when`(keyGenerator.generateKeys(10)).thenReturn(inputData)
+            val keyStore = InMemoryKeyStore(keyGenerator, 10)
+            keyStore.getKey()
+        }
+        Assert.isTrue(error.message == "Error populating cache", "Error message is incorrect")
     }
 }
